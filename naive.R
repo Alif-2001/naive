9d287f78f16278073b7d0eb7fb649b50ec7aa626
Alif-2001 <alifmerchant86@gmail.com>
2023-11-19 17:05:10 -0500

    update

--- naive.R
+++ naive.R
@@ -12,27 +12,36 @@ library(arrow)
 library(caret)
 
 # Read Data
+data <- read_parquet('AMRData.parquet')
+head(data)
 
-data <- read_parquet('../AMRData.parquet')[c(6, 7, 13)]
+
+data <- read_parquet('AMRData.parquet')[c(3, 6, 7, 12, 13, 14)]
 
 head(data)
 
+data$county <- as.factor(data$county)
 data$order_month <- as.factor(data$order_month)
 data$org_standard <- as.factor(data$org_standard)
 data$species <- as.factor(data$species)
+data$source <- as.factor(data$source)
 
 str(data)
 
 set.seed(1)
-index <- createDataPartition(data$org_standard, p = 0.6, list=FALSE)
+index <- createDataPartition(data$org_standard, p = 0.7, list=FALSE)
 train <- data[index, ]
 test <- data[-index, ]
 
 
 naive <- naiveBayes(org_standard~., data=train)
-
-predict <- predict(naive, test[-1])
+predict <- predict(naive, test)
 predictions<-as.data.frame(predict)
 
 con.mat<-confusionMatrix(data=as.factor(predictions$predict), reference = test$org_standard)
 print(con.mat)
+
+example_input <- c("New York", "1", "CANINE", "ABDOMINAL", "3")
+example <- predict(naive, example_input, "raw")
+
+head(example)
