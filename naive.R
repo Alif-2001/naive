036960600b9ad770e9cfdf9216f39e7bd5fc5510
alif <alifmerchant86@gmail.com>
2023-11-19 23:16:29 -0500

    Completed

--- naive.R
+++ naive.R
@@ -10,14 +10,13 @@ remove(list=ls())
 library(e1071)
 library(arrow)
 library(caret)
+library(dplyr)
 
 # Read Data
-data <- read_parquet('AMRData.parquet')
-head(data)
 
-
-data <- read_parquet('AMRData.parquet')[c(3, 6, 7, 12, 13, 14)]
-
+data <- read_parquet('AMRData.parquet')[c(3, 6, 7, 12, 13)]
+#data <- read_parquet('AMRData.parquet')[c(6, 7, 13)]
+data <- na.omit(data)
 head(data)
 
 data$county <- as.factor(data$county)
@@ -33,15 +32,39 @@ index <- createDataPartition(data$org_standard, p = 0.7, list=FALSE)
 train <- data[index, ]
 test <- data[-index, ]
 
+# Naive Bayes Model
 
 naive <- naiveBayes(org_standard~., data=train)
+
 predict <- predict(naive, test)
 predictions<-as.data.frame(predict)
 
 con.mat<-confusionMatrix(data=as.factor(predictions$predict), reference = test$org_standard)
 print(con.mat)
 
-example_input <- c("New York", "1", "CANINE", "ABDOMINAL", "3")
-example <- predict(naive, example_input, "raw")
+# Example test run
 
-head(example)
+example_input <- data.frame(
+  county = "New York",
+  order_month = "5",
+  species = "CANINE",
+  source = "CYSTOCENTESIS"
+)
+example <- predict(naive, example_input, "raw")
+example <- as.data.frame(example)
+
+selected_columns <- example %>%
+  select_if(~all(.>=0.01))
+
+other <- c(1 - rowSums(selected_columns))
+selected_columns <- cbind(selected_columns, other)
+cls <- colnames(selected_columns)
+selected_columns <- as.matrix(selected_columns)
+pie(selected_columns, labels = cls, col = rainbow(length(cls)))
+
+
+
+
+
+
+
